63f1d6b45e34de932c4b423bcb63c119680b8db4
TuanDang <dangtuannoone@gmail.com>
2018-01-02 17:11:03 +0700

    a

---
+++ WebApplication1/Areas/admin/Models/model/CauHoi.cs
@@ -0,0 +1,41 @@
+﻿using DBEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebThiB1Online.Areas.admin.Models
+{
+    public class CauHoi
+    {
+        public int ID { get; set; }
+        public string TieuDe { get; set; }
+        public string DapAnA { get; set; }
+        public string DapAnB { get; set; }
+        public string DapAnC { get; set; }
+        public string DapAnD { get; set; }
+        public string DapAn { get; set; }
+        private static List<CauHoi> lst = LayDSCauHoi(1);
+        public static  List<CauHoi> LayDSCauHoi(int MaTopic)
+        {
+            List<CauHoi> lstTam = new List<CauHoi>();
+            using (B1OnlineEntities context = new B1OnlineEntities())
+            {
+               var c= context.CauHois.ToList
+            };
+            return lstTam;
+        }
+        public static bool deleteCauHoi(int ID)
+        {
+            int i = CauHoi.lst.FindIndex(x => x.ID == ID);
+            if (i > -1)
+            {
+                CauHoi.lst.RemoveAt(i);
+                return true;
+            }
+            else return false;
+        }
+    }
+  
+
+}
